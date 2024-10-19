53bc651794c34b6d5cae803ffe88eb1f4500d2c1
gabtdiaz <gabriella.thorisson@gmail.com>
2024-10-19 12:24:35 +0200

    Added WorkOutDetailsWindowViewModel

--- FitApp/ViewModel/WorkoutDetailsWindowViewModel.cs
+++ FitApp/ViewModel/WorkoutDetailsWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FitApp.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,19 @@ using System.Threading.Tasks;
 
 namespace FitApp.ViewModel
 {
-    public class WorkoutDetailsWindowViewModel
+    public class WorkoutDetailsWindowViewModel 
     {
+        // Egenskaper
+        public Workout Workout {  get; set; }
+
+        // Konstruktor
+        public WorkoutDetailsWindowViewModel(Workout workout)
+        {
+            Workout = workout; 
+        }
+
+        // Metoder
+        public void EditWorkout() { }
+        public void SaveWorkout() { }
     }
 }
