cc376ec52875561d23e7cfefbbb36b7d1ae789aa
gabtdiaz <gabriella.thorisson@gmail.com>
2024-10-21 21:45:37 +0200

    Changes in WorkoutsWindowViewModel

--- FitApp/ViewModel/WorkoutsWindowViewModel.cs
+++ FitApp/ViewModel/WorkoutsWindowViewModel.cs
@@ -11,18 +11,21 @@ namespace FitApp.ViewModel
     public class WorkoutsWindowViewModel : MainWindowViewModel
     {
         // Egenskaper
-        new User User = new User();
+        public User User {  get; set; }
         new List<Workout> WorkoutList = new List<Workout>();
+        public Workout selectedWorkout { get; set; }
+ 
+        
 
         // Konstruktor
-        public WorkoutsWindowViewModel(User User, List<Workout> Workoutlist, string UsernameInput, string PasswordInput) 
+        public WorkoutsWindowViewModel(User currentUser) 
         {
-            this.User = User;
+            User = currentUser;
             WorkoutList = new List<Workout>
         {
             new CardioWorkout { Type = "Cardio", Distance = 5, Duration = new TimeSpan(0, 30, 0), CaloriesBurned = 300 },
             new StrengthWorkout { Type = "Strength", Repetitions = 10, Duration = new TimeSpan(0, 45, 0), CaloriesBurned = 400 }
-        }; // tilldela direkt.
+        }; // tilldela direkt?
         }
 
         // Metoder
