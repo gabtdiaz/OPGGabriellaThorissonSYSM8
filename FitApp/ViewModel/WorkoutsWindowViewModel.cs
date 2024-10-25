6accda1be4b89a3cfb20f731548b9d143d34a904
gabtdiaz <gabriella.thorisson@gmail.com>
2024-10-25 18:05:28 +0200

    grid changes in  AddWorkoutWIndow

--- FitApp/ViewModel/WorkoutsWindowViewModel.cs
+++ FitApp/ViewModel/WorkoutsWindowViewModel.cs
@@ -76,8 +76,8 @@ namespace FitApp.ViewModel
             Workouts = new ObservableCollection<Workout>
             // Skapar träningspass
         {
-            new CardioWorkout { Type = "HIIT Run", Duration = new TimeSpan(0, 30, 0), CaloriesBurned = 400, DateTime = new DateTime(2024, 10, 24, 08, 00, 0)},
-            new StrengthWorkout { Type = "Full Body Strength", Duration = new TimeSpan(0, 45, 0), CaloriesBurned = 300, DateTime = new DateTime(2024, 10, 20, 18, 30, 0)}
+            new CardioWorkout { Type = "HIIT Run", Duration = new TimeSpan(0, 30, 0), CaloriesBurned = 400, DateTime = new DateTime(2024, 10, 24, 08, 00, 0), Notes = ""},
+            new StrengthWorkout { Type = "Full Body Strength", Duration = new TimeSpan(0, 45, 0), CaloriesBurned = 300, DateTime = new DateTime(2024, 10, 20, 18, 30, 0), Notes = ""}
         };
             // Commands
             AddWorkoutCommand = new RelayCommand(AddWorkout);
